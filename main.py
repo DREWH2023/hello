4d0e136ac554aa86d80ccfb29e432080896f01f1
DREWH2023 <77160372+DREWH2023@users.noreply.github.com>
2021-01-08 09:53:57 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,5 @@
+
+basic.show_string("Hello! how are you")
+basic.show_icon(IconNames.HEART)
+basic.show_string("Stop the 3 sec timer!")
+game.start_countdown(3000)
---
+++ main.ts
@@ -0,0 +1,4 @@
+basic.showString("Hello! how are you")
+basic.showIcon(IconNames.Heart)
+basic.showString("Stop the 3 sec timer!")
+game.startCountdown(3000)
