209c44f4ea0d38586f9282150413c208089da35a
DREWH2023 <77160372+DREWH2023@users.noreply.github.com>
2021-01-10 20:55:30 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
 
-basic.show_string("Hello! how are you")
-basic.show_icon(IconNames.HEART)
-basic.show_string("Stop the 3 sec timer!")
-game.start_countdown(3000)
+
+def on_gesture_shake():
+    pass
+input.on_gesture(Gesture.Shake, on_gesture_shake)
+
+def on_button_pressed_a():
+    workout()
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    workout()
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def workout():
+    if input.on_gesture(Gesture.Shake, on_gesture_shake):
+        basic.show_string("5 Jumping Jacks!")
+    elif input.on_button_pressed(Button.A, on_button_pressed_a):
+        basic.show_string("10 Push-Ups!")
+    elif input.on_button_pressed(Button.B, on_button_pressed_b):
+        ("10 Burpees!")
+
+workout()
+basic.show_string("Ready to work? Lets start")
+basic.show_string("Shake, press B, or press A!")
+
+
+
+
--- main.ts
+++ main.ts
@@ -1,4 +1,29 @@
-basic.showString("Hello! how are you")
-basic.showIcon(IconNames.Heart)
-basic.showString("Stop the 3 sec timer!")
-game.startCountdown(3000)
+function on_gesture_shake() {
+    
+}
+
+input.onGesture(Gesture.Shake, on_gesture_shake)
+function on_button_pressed_a() {
+    workout()
+}
+
+input.onButtonPressed(Button.A, on_button_pressed_a)
+function on_button_pressed_b() {
+    workout()
+}
+
+input.onButtonPressed(Button.B, on_button_pressed_b)
+function workout() {
+    if (input.onGesture(Gesture.Shake, on_gesture_shake)) {
+        basic.showString("5 Jumping Jacks!")
+    } else if (input.onButtonPressed(Button.A, on_button_pressed_a)) {
+        basic.showString("10 Push-Ups!")
+    } else if (input.onButtonPressed(Button.B, on_button_pressed_b)) {
+        /** 10 Burpees! */
+    }
+    
+}
+
+workout()
+basic.showString("Ready to work? Lets start")
+basic.showString("Shake, press B, or press A!")
